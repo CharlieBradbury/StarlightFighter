e06320f29ab97d4bd0f699520343043e6d1d4466
Oscar González <osdagoso@hotmail.com>
2016-04-22 00:08:07 -0500

    Menu Principal provisional agregado.

---
+++ Assets/Scripts/ExitFromMainMenuOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitFromMainMenuOnClick : MonoBehaviour {
+
+	// Exits the application upon triggering
+	public void ExitOnClick () {
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	// Declare a game object for the loading image
+	public GameObject goLoadingImage;
+	
+	// Shows the loading image while loading a scene
+	public void LoadLevel(int iLevel)
+	{
+		goLoadingImage.SetActive(true);
+		SceneManager.LoadScene(iLevel);
+	}
+}
